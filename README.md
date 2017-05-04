cf67058e308160a23aae726c4bd5c642a5e94937
Felix Erlacher <felix.erlacher@ccs-labs.org>
2017-05-04 16:26:25 +0200

    fixed hex conversion bugs, printing also original content

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ UNDER CONSTRUCTION
 
 Parses Snort rules from a rule file, puts content in a struct and ev. prints it.
 
-Build it by executing "g++ snorRuleParser.cpp"
+Build it by executing "g++ snortRuleParser.cpp"
--- snortRuleParser.cpp
+++ snortRuleParser.cpp
@@ -20,17 +20,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stdexcept>
 #include <stdlib.h>
 #include <stdint.h>
 #include <vector>
+#include <locale>
 
 
 class ruleBody{
     public:
     std::string msg;
-    bool negatedContent;
-    //string contentOriginal;
+    std::vector<bool> negatedContent;
+    std::vector<std::string> contentOriginal;
     std::vector<bool> containsHex;
     std::vector<std::string> content;
     std::string sid;
@@ -48,10 +48,10 @@ std::size_t bodyStartPosition;
 std::size_t startPosition;
 std::size_t endPosition;
 
-
 //TODO
 //void parseHTTP restrictors
 //parse pcre's
+//convert more than the first 128 ascii character (minus nonprintable chars)
 /**
 * writes error message to stderr
 */
@@ -63,19 +63,22 @@ void parsingError(int line, std::string parsingPart){
 *prints snortRule struct to stdout
 */
 void printSnorRule(snortRule* rule){
-    fprintf(stdout,"Message %s\n",rule->body.msg.c_str());
+    fprintf(stdout,"Message: %s\n",rule->body.msg.c_str());
     fprintf(stdout,"Header: %s\n",rule->header.c_str());
-    if(rule->body.negatedContent){
-        fprintf(stdout,"NOT ");
-    }
 
     for(int i=0;i<rule->body.content.size();i++){
+        if(rule->body.negatedContent[i]==true){
+            fprintf(stdout,"NOT ");
+        }
+        fprintf(stdout,"ContentOriginal:\t%s\n",rule->body.contentOriginal[i].c_str());
         if(rule->body.containsHex[i]==true){
-            fprintf(stdout,"Content (hex converted): %s\n",rule->body.content[i].c_str());
+            fprintf(stdout,"Content (hex converted):%s\n",rule->body.content[i].c_str());
         }else{
-            fprintf(stdout,"Content: %s\n",rule->body.content[i].c_str());
+            fprintf(stdout,"Content:\t\t%s\n",rule->body.content[i].c_str());
         }
     }
+
+
     fprintf(stdout,"SID: %s\n",rule->body.sid.c_str());
     fprintf(stdout,"SID rev: %s\n",rule->body.rev.c_str());
     fprintf(stdout,"\n");
@@ -95,7 +98,7 @@ void parseMsg(std::string* line, int* linecounter, snortRule* tempRule){
 }
 
 /**
-*parses the rule header from given line and writes it to given snortRule struct
+*parses the rule header from given line and writes it to given snortRule class
 */
 void parseHeader(std::string* line, int* linecounter, snortRule* tempRule){
     bodyStartPosition=line->find("(");
@@ -107,15 +110,15 @@ void parseHeader(std::string* line, int* linecounter, snortRule* tempRule){
 }
 
 /**
-* parses rule content from given line and writes it to given tempRule struct
+* parses rule content (also multiple contents) from given line and writes it to given tempRule class in the corresponding vector of contents,
 * it also converts hex characters to ascii characters, if possible, in not it omits them in the output content
 */
 void parseContent(std::string* line, int* linecounter, snortRule* tempRule){
-//TODO do it for multiple contents
     std::size_t hexStartPosition;
-    std::size_t hexEndPosition;
+    std::size_t hexEndPosition=0;
     std::string hexContent;
-    std::string content;
+    std::string contentOrig;
+    std::string contentHexFree;
     std::string tempContent;
     std::string byte;
     char tempChar;
@@ -132,45 +135,54 @@ void parseContent(std::string* line, int* linecounter, snortRule* tempRule){
 
     //loop to detect multiple content keywords, same check as above is repeated, will be true first time for sure, but we dont want to call parsingError the other times
     while(startPosition!=(std::string::npos+8)&&endPosition!=std::string::npos){
+        contentHexFree="";
         //check if content is negated BWARE: than also modifiers are negated!!!
         if(line->substr(startPosition,1)=="!"){
-            tempRule->body.negatedContent=1;
-            fprintf(stdout,"WARNING: Line %d contains negated content, this negates content modifiers to but has not been implementet\n",*linecounter);
+            tempRule->body.negatedContent.push_back(true);
+            fprintf(stdout,"WARNING: Line %d contains negated content, this negates content modifiers too but has not been implementet\n",*linecounter);
         }else{
-            tempRule->body.negatedContent=0;
+            tempRule->body.negatedContent.push_back(false);
         }
-        content=line->substr(startPosition,(endPosition-startPosition));
+        contentOrig=line->substr(startPosition,(endPosition-startPosition));
         //cut away quotes
-        content=content.substr(1,(content.size()-2));
+        contentOrig=contentOrig.substr(1,(contentOrig.size()-2));
+
+        //for debug and functionality check purposes write original content
+        tempRule->body.contentOriginal.push_back(contentOrig);
         //check if it contains hex
-        hexStartPosition=content.find("|");
+        hexStartPosition=contentOrig.find("|");
 
         //is checked again below, but necessery here too
-        if(hexStartPosition!=std::string::npos||content.find("|",hexStartPosition+1)!=std::string::npos){
-            tempRule->body.containsHex[contentCounter]=1;
+        if(hexStartPosition!=std::string::npos||contentOrig.find("|",hexStartPosition+1)!=std::string::npos){
+            tempRule->body.containsHex.push_back(1);
+            //if it contains hex than add hexfree content before hex content to contentHexFree
+            contentHexFree=contentHexFree+contentOrig.substr(0,hexStartPosition);
         }else{
-            tempRule->body.containsHex[contentCounter]=0;
+            tempRule->body.containsHex.push_back(0);
+            //if it does not contain hex at all add it now to hex free content:
+            contentHexFree=contentHexFree+contentOrig;
         }
         //find all hex codes and convert them to ascii
         while(hexStartPosition!=std::string::npos){
-            hexEndPosition=content.find("|",hexStartPosition+1);
+            hexEndPosition=contentOrig.find("|",hexStartPosition+1);
             if(hexEndPosition==std::string::npos){
-                fprintf(stdout,"content no hex=%s",content.c_str());
+                fprintf(stdout,"Debug: content no hex=%s, already converted content: %s\n",contentOrig.c_str(),contentHexFree.c_str());
                 parsingError(*linecounter,"hex content (no termination sign)");
                 exit(0);
             }
-
-                //already cutting off first pipe sign
-            hexContent=content.substr(hexStartPosition+1,(hexEndPosition-hexStartPosition)-1);
-                //remove spaces from hex string
+            //copying hex string and cutting off first pipe sign
+            hexContent=contentOrig.substr(hexStartPosition+1,(hexEndPosition-hexStartPosition)-1);
+            //remove spaces from hex string
             tempPosition=hexContent.find(" ");
             while(tempPosition!=std::string::npos){
                 hexContent.erase(tempPosition,1);
                 tempPosition=hexContent.find(" ",tempPosition);
             }
-                //transform hex to ascii
+
             std::string asciiString;
-            for (uint16_t i=0;i<(hexContent.length()/2);i++){
+            //transform hex to ascii loop, as it always consumes two chars we have to move over two chars after every loop
+            //todo ev. convert line break/line feed hex codes to OS specific signs
+            for (uint16_t i=0;i<(hexContent.length());i=i+2){
                 char * pEnd;
                 byte = hexContent.substr(i,2);
                 tempChar=(char) (int)strtol(byte.c_str(), &pEnd, 16);
@@ -178,16 +190,22 @@ void parseContent(std::string* line, int* linecounter, snortRule* tempRule){
                     asciiString.push_back(tempChar);
                 }//if not printable ignore char
             }
-            //replace hex content with ascii content:
-            content=content.substr(0,hexStartPosition) + asciiString + content.substr(hexEndPosition+1,(hexEndPosition+1+content.length()));
-            //content now does not contain previos hex anymore
-            hexStartPosition=content.find("|",0);
+            //adding converted string to content
+            contentHexFree=contentHexFree+asciiString;
+            //content now does not contain previous hex anymore, but may contain pipe sign if converted from hex
+            hexStartPosition=contentOrig.find("|",hexEndPosition+1);
+            //if more hex, than get content in between last and next hex string
+            if(hexStartPosition!=std::string::npos){
+                contentHexFree=contentHexFree+contentOrig.substr(hexEndPosition+1,hexStartPosition-hexEndPosition-1);
+            //if this was last hex (and here we had at least one hex string) add possible tailing hex free string to content
+            }else{
+                contentHexFree=contentHexFree+contentOrig.substr(hexEndPosition+1,contentOrig.size()-hexEndPosition+1);
+            }
         }//while hex loop
-        //now it should not contain hex anymore
-        tempRule->body.content.push_back(content);
+        //add the summed up content to the rule class
+        tempRule->body.content.push_back(contentHexFree);
         //erase content keyword, so that loop can find next content keyword or break
         line->erase(startPosition-8,8);
-
         startPosition=line->find("content:",bodyStartPosition)+8;
         endPosition=line->find(";",startPosition);
         contentCounter++;
@@ -223,18 +241,19 @@ int main (int argc, char* argv[]) {
     snortRule tempRule;
     //hardly any rule will use more than 15 content keywords
     tempRule.body.content.reserve(15);
+    tempRule.body.contentOriginal.reserve(15);
     tempRule.body.containsHex.reserve(15);
+    tempRule.body.negatedContent.reserve(15);
     //disable buffering on stdout:
     setbuf(stdout, NULL);
 
     // Check the number of parameters
     if (argc != 2) {
-        fprintf(stderr,"Usage: \"%s filename\", where filename is the path to a file containing Snort rules\n.",argv[0]);
+        fprintf(stderr,"This is a rule parser for Snort IDS rules.\n\nUsage:\n\"%s filename\"\nwhere 'filename' is the path to a file containing Snort rules.\n",argv[0]);
         exit(1);
     }
 
     std::ifstream ruleFile (argv[1]);
-
     if (ruleFile.is_open())
     {
         //one line is one snort rule
@@ -257,8 +276,8 @@ int main (int argc, char* argv[]) {
             }
             tempRule.body.containsHex.clear();
             tempRule.body.content.clear();
-            tempRule.body.content.reserve(15);
-            tempRule.body.containsHex.reserve(15);
+            tempRule.body.negatedContent.clear();
+            tempRule.body.contentOriginal.clear();
     }
     ruleFile.close();
     }else{
